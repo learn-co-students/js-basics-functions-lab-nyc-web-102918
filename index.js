d1df25770b9f76648a166b913ae9596354b20c90
gavinmoore36 <gavizoid16@gmail.com>
2019-01-07 14:13:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  const hq = 42
+    if(location < hq) {
+      return hq - location
+    }
+    else if (location > hq) {
+      return location - hq
+    }
+}
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264
+}
+function distanceTravelledInFeet(a, b) {
+  if(a < b) {
+    return (b - a) * 264
+  }
+  else if (a > b) {
+    return (a - b) * 264
+  }
+}
+function calculatesFarePrice(start, destination) {
+  const totalDistance = distanceTravelledInFeet(start, destination)
+  if(totalDistance < 400) {
+    return 0
+  }
+  else if (totalDistance > 400 && totalDistance < 2000) {
+    return (totalDistance - 400) * .02
+  }
+  else if (totalDistance > 2000 && totalDistance < 2500) {
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
